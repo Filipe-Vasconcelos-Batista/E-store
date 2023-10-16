42b6f03ae43e54a562f0b50a455810d615ea8e9e
Filipe Batista <filipevbatista@live.com>
2023-10-16 11:20:16 +0200

    start of the project

---
+++ Documentos/Coding_Turing_college/sprint_2.2/2.2.5/tipying.js
@@ -0,0 +1,4 @@
+//fetch the api for the tipying
+//make it display
+//make it so you can write it somewhere, lets start with a area text and go from there
+//make the time start counting (display on the screen the remaining time)
