c1575ec4215bc7f9575dcd58efd6a73ad24b79fd
Filipe Vasconcelos  Batista <124799878+Filipe-Vasconcelos-Batista@users.noreply.github.com>
2024-03-16 07:07:03 +0100

    Delete Documentos/e-store directory

--- Documentos/e-store/packages/client/e2e/test-2.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-  expect(page.getByRole('heading', { name: 'Our Catalog' }).textContent).toBe('Our Catalog');
-});
