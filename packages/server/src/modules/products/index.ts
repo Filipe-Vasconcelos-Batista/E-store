5c293e530cf4f5ef8b87f287a20074d165c738fd
Filipe Batista <filipevbatista@live.com>
2024-03-20 15:12:50 +0100

    feat:can now search the entire Product table with/without offset and limit

--- packages/server/src/modules/products/index.ts
+++ packages/server/src/modules/products/index.ts
@@ -1,8 +1,10 @@
 import { router } from '@server/trpc'
 import create from './insert'
 import findById from './findById'
+import findAll from './findAll'
 
 export default router({
   create,
   findById,
+  findAll,
 })
